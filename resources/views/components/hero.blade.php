3a979170834eced623411b8188900d52182002f1
KELSZ22 <kelgithub@gmail.com>
2024-12-17 17:57:06 +0800

    add all

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,13 @@
+<div class="bg-red-700 max-h-full p-10">
+<div class="flex justify-center">
+       <div class="flex flex-col font-mono font-semibold justify-center mr-24">
+                <h1 class=" text-white text-8xl"> WELCOME TO </h1>
+                <h1 class="text-white text-8xl">MY WEBSITE</h1>
+                <h2 class="text-3xl text-white"> Hi I'm Michael Z. Sabino</h2>
+                <h3 class="text-2xl text-sky-400"> Web Developer</h3>
+                <button class="bg-blue-400 text-white w-40 p-2 rounded-xl mt-5 transition-all hover:bg-transparent hover:border-2 border-white "> Get Started</button>
+       </div>
+
+        <img class="h-1/3" src="{{url('images/manpower.png')}}" alt="manpower.png">
+    
+</div></div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CMS WEBSITE</title>
+    @vite('resources/css/app.css')
+
+</head>
+<body>
+
+    <h1>
+        <x-header/>
+    </h1>
+
+        <x-hero/>
+
+    <x-footer/>
+</body>
+</html>
